9b15153bbc91ae9f9db499584f214571b62e2af3
braiatenebras <bryan.fagundes@escola.pr.gov.br>
2025-03-31 10:45:20 -0300

    começando

--- BryanMobile/app/(tabs)/index.tsx
+++ BryanMobile/app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, View } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -10,34 +9,38 @@ export default function HomeScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
-        <Image
-          source={require('@/assets/images/images.jpeg')}
-          style={styles.reactLogo}
-
-        />
-
+        <View style={styles.headerContainer}>
+          <Image
+            source={require('@/assets/images/images.png')}
+            style={styles.reactLogo}
+          />
+          <Image
+            source={require('@/assets/images/images2.png')}
+            style={styles.secondImage}
+          />
+        </View>
       }>
+      {/* Resto do conteúdo */}
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Olá</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Bem Vindo </ThemedText>
+        <ThemedText type="subtitle">Bem Vindo ao meu site! </ThemedText>
         <ThemedText>
-          <ThemedText type="defaultSemiBold">Oieee </ThemedText>
+          <ThemedText type="defaultSemiBold">Meu nome é Bryan Kauan e atualmente estou cursando o 3° ano do Ensino
+            Médio no Colégio Estadual Civíco Militar Professor Guido Arzua </ThemedText>
           {' '}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">oieee</ThemedText>
-        <ThemedText>
-          dasdasdsa        </ThemedText>
+        <ThemedText type="subtitle">Sobre mim 😂 </ThemedText>
+        <ThemedText> Atualmente tenho 16 anos (15/09/08)  </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle"> aqui aqui </ThemedText>
         <ThemedText>
           teste teste {' '}
-
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -54,11 +57,23 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
+  headerContainer: {
+    position: 'relative',
+    width: '100%',
+    height: 245, 
+  },
   reactLogo: {
-    height: 250,
-    width: 500,
-    bottom: 0,
+    height: 400,
+    width: 300,
+    bottom: -100,
     left: 0,
     position: 'absolute',
+  },
+  secondImage: {
+    height: 350, 
+    width: 300,
+    position: 'absolute',
+    right: -10, 
+    top: -45,   
   },
 });
